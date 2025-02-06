a0abca3be5bc5b0158be2418034de09444f88eab
varun050201 <varunkorlapati@gmail.com>
2025-02-06 13:57:01 -0500

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,7 +5,6 @@ import './Projects.css';
 const Projects = () => {
   return (
     <div>
-      <Navbar />
       <div className="projects-content">
         <h1>Projects</h1>
         <p>Details about your projects.</p>
