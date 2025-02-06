8537a4fa4f0f9d8bcc96bc8256cc989ad07c14a9
varun <varunkorlapati@gmail.com>
2025-02-06 13:22:14 -0500

    Update routing and deploy configuration

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
-import Experience from './components/Experience';
-import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Resume from './components/Resume';
+import Experience from './components/Experience';
+import Navbar from './components/Navbar';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <Router>
+    <Router basename="/varun-portfolio">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/projects" element={<Projects />} />
         <Route path="/skills" element={<Skills />} />
         <Route path="/resume" element={<Resume />} />
+        <Route path="/experience" element={<Experience />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
